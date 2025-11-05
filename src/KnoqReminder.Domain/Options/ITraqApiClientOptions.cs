f7a348f0e619d1cd70a5c6e9b83151e987384b9e
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2025-11-06 06:52:05 +0900

    🎨 fmt

--- src/KnoqReminder.Domain/Options/ITraqApiClientOptions.cs
+++ src/KnoqReminder.Domain/Options/ITraqApiClientOptions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KnoqReminder.Domain.Options;
+﻿namespace KnoqReminder.Domain.Options;
 
 public interface ITraqApiClientOptions
 {
