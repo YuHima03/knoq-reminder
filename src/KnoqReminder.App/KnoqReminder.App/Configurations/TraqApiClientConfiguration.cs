445273ddaa29b5572fb5932b5ff775061d19026f
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2025-11-06 06:08:58 +0900

    ✨ ITraqApiClientOptions

--- src/KnoqReminder.App/KnoqReminder.App/Configurations/TraqApiClientConfiguration.cs
+++ src/KnoqReminder.App/KnoqReminder.App/Configurations/TraqApiClientConfiguration.cs
@@ -1,9 +1,20 @@
-﻿namespace KnoqReminder.App.Configurations;
+﻿using KnoqReminder.Domain.Options;
 
-public class TraqApiClientConfiguration
+namespace KnoqReminder.App.Configurations;
+
+public class TraqApiClientConfiguration : ITraqApiClientOptions
 {
     public const string EnvironmentPrefix = "TRAQ_";
 
     [ConfigurationKeyName(EnvironmentPrefix + "BASE_ADDRESS")]
-    public string? BaseUrl { get; set; }
+    public string BaseUrl
+    {
+        get => _baseUrl;
+
+        set => _baseUrl = Uri.IsWellFormedUriString(value, UriKind.Absolute) ? value : string.Empty;
+    }
+    string _baseUrl = string.Empty;
+
+    [ConfigurationKeyName(EnvironmentPrefix + "ACCESS_TOKEN")]
+    public string AccessToken { get; set; } = string.Empty;
 }
---
+++ src/KnoqReminder.Domain/Options/ITraqApiClientOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnoqReminder.Domain.Options;
+
+public interface ITraqApiClientOptions
+{
+    string BaseUrl { get; }
+
+    string AccessToken { get; }
+}
