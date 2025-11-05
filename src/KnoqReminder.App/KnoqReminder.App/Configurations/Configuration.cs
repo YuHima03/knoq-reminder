671f47e6519c8245e3c2a5ea221c4f09d80644fd
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2025-11-05 22:47:11 +0900

    ✨ extension class for configuring options

---
+++ src/KnoqReminder.App/KnoqReminder.App/Configurations/Configuration.cs
@@ -0,0 +1,26 @@
+﻿using KnoqReminder.Domain.Options;
+using Microsoft.Extensions.Options;
+
+namespace KnoqReminder.App.Configurations;
+
+static class Configuration
+{
+    public static IServiceCollection ConfigureAppOptions(this IServiceCollection services, IConfiguration config)
+    {
+        return services
+            .Configure<DbConnectionOptions, MariaDbConnectionConfiguration>(config);
+    }
+
+    /// <summary>
+    /// Registers a <typeparamref name="TOptionsImplement"/> configuration as of the <typeparamref name="TOptions"/> type.
+    /// </summary>
+    static IServiceCollection Configure<TOptions, TOptionsImplement>(this IServiceCollection services, IConfiguration config)
+        where TOptions : class
+        where TOptionsImplement : class, TOptions
+    {
+        return services
+            .AddOptions()
+            .AddSingleton((IOptionsChangeTokenSource<TOptions>)new ConfigurationChangeTokenSource<TOptionsImplement>(config))
+            .AddSingleton((IConfigureOptions<TOptions>)new ConfigureOptions<TOptionsImplement>(config.Bind));
+    }
+}
