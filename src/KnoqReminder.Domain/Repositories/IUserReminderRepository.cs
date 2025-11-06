80dc1bdf85d984310241575900c56b14ec0c9552
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2025-11-06 17:04:55 +0900

    ✨ IUserReminderRepository

---
+++ src/KnoqReminder.Domain/Repositories/IUserReminderRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KnoqReminder.Domain.Models;
+
+namespace KnoqReminder.Domain.Repositories;
+
+public interface IUserReminderRepository : IRepositoryBase
+{
+    ValueTask<UserReminder> GetUserReminderAsync(Guid id, CancellationToken cancellationToken = default);
+
+    ValueTask<UserReminder> GetUserReminderByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
+
+    ValueTask<UserReminder[]> GetUserRemindersByAotReminderTimeAsync(TimeSpan timeSpanFrom, TimeSpan timeSpan, CancellationToken cancellationToken = default);
+
+    ValueTask<UserReminder[]> GetUserRemindersByDailyReminderTimeAsync(TimeOnly timeFrom, TimeSpan timeSpan, CancellationToken cancellationToken = default);
+}
