abf5b42e295ea035c44e247049874637124f00d7
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2025-11-11 05:36:41 +0900

    💡 add doc comment

--- src/KnoqReminder.Domain/Models/UserReminder.cs
+++ src/KnoqReminder.Domain/Models/UserReminder.cs
@@ -1,5 +1,16 @@
 ﻿namespace KnoqReminder.Domain.Models;
 
+/// <summary>
+/// Represents a setting for event reminders to a user.
+/// </summary>
+/// <param name="Id"></param>
+/// <param name="UserId"></param>
+/// <param name="RemindsWhenAbsent"></param>
+/// <param name="RemindsFreeEvents"></param>
+/// <param name="AheadOfTimeReminderTimes"></param>
+/// <param name="DailyReminderTimes"></param>
+/// <param name="CreatedAt"></param>
+/// <param name="UpdatedAt"></param>
 public record UserReminder(
     Guid Id,
     Guid UserId,
--- src/KnoqReminder.Domain/Models/UserReminderAddOrUpdateRequest.cs
+++ src/KnoqReminder.Domain/Models/UserReminderAddOrUpdateRequest.cs
@@ -1,5 +1,13 @@
 ﻿namespace KnoqReminder.Domain.Models;
 
+/// <summary>
+/// Represents a configuration for adding a new item to or updating an existing item in a set of <see cref="UserReminder"/>s.
+/// </summary>
+/// <param name="UserId"></param>
+/// <param name="RemindsWhenAbsent"></param>
+/// <param name="RemindsFreeEvents"></param>
+/// <param name="AheadOfTimeReminderTimes"></param>
+/// <param name="DailyReminderTimes"></param>
 public record UserReminderAddOrUpdateRequest(
     Guid? UserId,
     ReminderKind? RemindsWhenAbsent,
