a4fb7dcdce2d280f833f15620ee13a464ff9bf2d
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2025-11-11 03:27:19 +0900

    ✨ new type: UserReminderAddOrUpdateRequest

---
+++ src/KnoqReminder.Domain/Models/UserReminderAddOrUpdateRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace KnoqReminder.Domain.Models;
+
+public record UserReminderAddOrUpdateRequest(
+    Guid? Id,
+    Guid UserId,
+    ReminderKind RemindsWhenAbsent,
+    ReminderKind RemindsFreeEvents,
+    AheadOfTimeReminderTime[] AheadOfTimeReminderTimes,
+    DailyReminderTime[] DailyReminderTimes
+    );
