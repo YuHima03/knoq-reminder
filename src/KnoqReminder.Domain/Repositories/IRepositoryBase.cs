befca7b4946cd94b9c03db066952fd5aecf0b9a8
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2025-11-11 01:29:33 +0900

    🎨 fmt

--- src/KnoqReminder.Domain/Repositories/IRepositoryBase.cs
+++ src/KnoqReminder.Domain/Repositories/IRepositoryBase.cs
@@ -1,9 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KnoqReminder.Domain.Repositories;
+﻿namespace KnoqReminder.Domain.Repositories;
 
 public interface IRepositoryBase : IAsyncDisposable, IDisposable;
