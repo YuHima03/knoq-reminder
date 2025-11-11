aff3018d000a455fc1e876d94501292a041a776a
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2025-11-11 17:22:16 +0900

    ✨ UserReminderConverter class

---
+++ src/KnoqReminder.Infrastructure/Database/Converters/UserReminderConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+using KnoqReminder.Domain.Models;
+
+namespace KnoqReminder.Infrastructure.Database.Converters;
+
+static class UserReminderConverter
+{
+    public static readonly Expression<Func<UserReminder, Domain.Models.UserReminder>> DtoToDomainExpression = dto => new(
+        dto.Id,
+        dto.UserId,
+        Enum.Parse<ReminderKind>(dto.RemindsWhenAbsent, true),
+        Enum.Parse<ReminderKind>(dto.RemindsFreeEvents, true),
+        dto.AheadOfTimeReminders.Select(ar => new AheadOfTimeReminderTime(ar.Duration)).ToArray(),
+        dto.DailyReminders.Select(dr => new DailyReminderTime(TimeOnly.FromTimeSpan(dr.Time))).ToArray(),
+        dto.CreatedAt,
+        dto.UpdatedAt);
+}
