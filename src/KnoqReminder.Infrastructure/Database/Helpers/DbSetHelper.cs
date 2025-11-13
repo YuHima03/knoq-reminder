278776af20504bd3793a4e418091cadf1f7111b9
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2025-11-14 02:48:21 +0900

    ✨ GetChangesSlow, ApplyChangesSlow method

--- src/KnoqReminder.Infrastructure/Database/Helpers/DbSetHelper.cs
+++ src/KnoqReminder.Infrastructure/Database/Helpers/DbSetHelper.cs
@@ -10,61 +10,130 @@ static class DbSetHelper
 {
     const int MaxStackAllocationBytes = 1024;
 
-    public static void ApplyChanges<TEntity, TValue>(this DbSet<TEntity> dbSet, scoped ReadOnlySpan<TValue> newState, scoped ReadOnlySpan<TEntity> previousState, Func<TEntity, TValue> valueConverter, Func<TValue, TEntity> entityFactory)
+    public static void ApplyChanges<TEntity, TValue>(
+        this DbSet<TEntity> dbSet,
+        scoped ReadOnlySpan<TValue> newState,
+        scoped ReadOnlySpan<TEntity> previousState,
+        Func<TEntity, TValue> valueConverter,
+        Func<TValue, TEntity> entityFactory
+    )
         where TEntity : class
-        where TValue : unmanaged, IComparable<TValue>, IEquatable<TValue>
+        where TValue : unmanaged, IComparable<TValue>
     {
         Guard.IsNotNull(dbSet);
         Guard.IsNotNull(valueConverter);
         Guard.IsNotNull(entityFactory);
-
         if ((newState.Length + previousState.Length) * Unsafe.SizeOf<IndexedValue<TValue>>() <= MaxStackAllocationBytes)
         {
             Span<IndexedValue<TValue>> buffer = stackalloc IndexedValue<TValue>[newState.Length + previousState.Length];
-            execCore(dbSet, newState, previousState, buffer, valueConverter, entityFactory);
+            DbSetHelperFileInternal.ApplyChangesCore(dbSet, newState, previousState, buffer, valueConverter, entityFactory);
         }
         else
         {
             using var bufferArray = RentArray.RentAndCreate<IndexedValue<TValue>>(newState.Length + previousState.Length);
-            var buffer = bufferArray.Span;
-            execCore(dbSet, newState, previousState, buffer, valueConverter, entityFactory);
+            DbSetHelperFileInternal.ApplyChangesCore(dbSet, newState, previousState, bufferArray.Span, valueConverter, entityFactory);
         }
+    }
 
-        static void execCore(
+    public static void ApplyChangesSlow<TEntity, TValue>(
+        this DbSet<TEntity> dbSet,
+        scoped ReadOnlySpan<TValue> newState,
+        scoped ReadOnlySpan<TEntity> previousState,
+        Func<TEntity, TValue> valueConverter,
+        Func<TValue, TEntity> entityFactory
+    )
+        where TEntity : class
+        where TValue : struct, IComparable<TValue>
+    {
+        Guard.IsNotNull(dbSet);
+        Guard.IsNotNull(valueConverter);
+        Guard.IsNotNull(entityFactory);
+        using var bufferArray = RentArray.RentAndCreate<IndexedValue<TValue>>(newState.Length + previousState.Length);
+        DbSetHelperFileInternal.ApplyChangesCoreSlow(dbSet, newState, previousState, bufferArray.Span, valueConverter, entityFactory);
+    }
+}
+
+file static class DbSetHelperFileInternal
+{
+    static void HandleChanges<TEntity, TValue>(
+        DbSet<TEntity> dbSet,
+        scoped ReadOnlySpan<TEntity> previousState,
+        scoped ReadOnlySpan<EnumerableChangesHelper.ValueWithChange<IndexedValue<TValue>>> changes,
+        Func<TValue, TEntity> entityFactory
+    )
+        where TEntity : class
+        where TValue : struct, IComparable<TValue>
+    {
+        foreach (var ((idx, val), chg) in changes)
+        {
+            switch (chg)
+            {
+                case EnumerableChangesHelper.Change.Added:
+                    dbSet.Add(entityFactory(val));
+                    break;
+                case EnumerableChangesHelper.Change.Removed:
+                    dbSet.Remove(previousState[idx]);
+                    break;
+            }
+        }
+    }
+
+    static void PrepareIndexed<TEntity, TValue>(
+        scoped ReadOnlySpan<TValue> newState,
+        scoped ReadOnlySpan<TEntity> previousState,
+        scoped Span<IndexedValue<TValue>> newIndexedState,
+        scoped Span<IndexedValue<TValue>> previousIndexedState,
+        Func<TEntity, TValue> valueConverter
+    )
+        where TValue : struct, IComparable<TValue>
+    {
+        newState.AsValueEnumerable()
+                    .Select((v, i) => new IndexedValue<TValue>(i, v))
+                    .CopyTo(newIndexedState);
+        previousState.AsValueEnumerable()
+            .Select((e, i) => new IndexedValue<TValue>(i, valueConverter(e)))
+            .CopyTo(previousIndexedState);
+    }
+
+    public static void ApplyChangesCore<TEntity, TValue>(
             DbSet<TEntity> dbSet,
             scoped ReadOnlySpan<TValue> newState,
             scoped ReadOnlySpan<TEntity> previousState,
             scoped Span<IndexedValue<TValue>> indexedStateBuffer,
             Func<TEntity, TValue> valueConverter,
-            Func<TValue, TEntity> entityFactory)
-        {
-            var newIndexedState = indexedStateBuffer[..newState.Length];
-            var previousIndexedState = indexedStateBuffer[newState.Length..];
-            newState.AsValueEnumerable()
-                            .Select((v, i) => new IndexedValue<TValue>(i, v))
-                            .CopyTo(newIndexedState);
-            previousState.AsValueEnumerable()
-                .Select((e, i) => new IndexedValue<TValue>(i, valueConverter(e)))
-                .CopyTo(previousIndexedState);
-            using var changes = newIndexedState.GetChanges(previousIndexedState, IndexedValueComparer<TValue>.Instance);
-            foreach (var ((idx, val), chg) in changes.Span)
-            {
-                switch (chg)
-                {
-                    case EnumerableChangesHelper.Change.Added:
-                        dbSet.Add(entityFactory(val));
-                        break;
-                    case EnumerableChangesHelper.Change.Removed:
-                        dbSet.Remove(previousState[idx]);
-                        break;
-                }
-            }
-        }
+            Func<TValue, TEntity> entityFactory
+    )
+        where TEntity : class
+        where TValue : unmanaged, IComparable<TValue>
+    {
+        var newIndexedState = indexedStateBuffer[..newState.Length];
+        var previousIndexedState = indexedStateBuffer[newState.Length..];
+        PrepareIndexed(newState, previousState, newIndexedState, previousIndexedState, valueConverter);
+        using var changes = newIndexedState.GetChanges(previousIndexedState, IndexedValueComparer<TValue>.Instance);
+        HandleChanges(dbSet, previousState, changes.Span, entityFactory);
+    }
+
+    public static void ApplyChangesCoreSlow<TEntity, TValue>(
+            DbSet<TEntity> dbSet,
+            scoped ReadOnlySpan<TValue> newState,
+            scoped ReadOnlySpan<TEntity> previousState,
+            scoped Span<IndexedValue<TValue>> indexedStateBuffer,
+            Func<TEntity, TValue> valueConverter,
+            Func<TValue, TEntity> entityFactory
+    )
+        where TEntity : class
+        where TValue : struct, IComparable<TValue>
+    {
+        var newIndexedState = indexedStateBuffer[..newState.Length];
+        var previousIndexedState = indexedStateBuffer[newState.Length..];
+        PrepareIndexed(newState, previousState, newIndexedState, previousIndexedState, valueConverter);
+        using var changes = newIndexedState.GetChangesSlow(previousIndexedState, IndexedValueComparer<TValue>.Instance);
+        HandleChanges(dbSet, previousState, changes.Span, entityFactory);
     }
 }
 
 file readonly struct IndexedValue<T>(int index, T Value)
-    where T : unmanaged, IComparable<T>
+    where T : struct, IComparable<T>
 {
     public int Index { get; init; } = index;
 
@@ -78,7 +147,7 @@ file readonly struct IndexedValue<T>(int index, T Value)
 }
 
 file class IndexedValueComparer<T> : Comparer<IndexedValue<T>>
-    where T : unmanaged, IComparable<T>
+    where T : struct, IComparable<T>
 {
     public static readonly IndexedValueComparer<T> Instance = new();
 
--- src/KnoqReminder.Infrastructure/Database/Helpers/EnumerableChangesHelper.cs
+++ src/KnoqReminder.Infrastructure/Database/Helpers/EnumerableChangesHelper.cs
@@ -30,52 +30,53 @@ static class EnumerableChangesHelper
 
     public static RentArray<ValueWithChange<T>> GetChanges<T>(this scoped ReadOnlySpan<T> current, scoped ReadOnlySpan<T> previous, IComparer<T>? comparer = default) where T : unmanaged
     {
-        comparer ??= Comparer<T>.Default;
-        if (previous.Length * 2 * Unsafe.SizeOf<T>() <= MaxStackAllocationBytes)
+        if (previous.Length * 2 * Unsafe.SizeOf<T>() > MaxStackAllocationBytes)
         {
-            Span<T> bufPrev = stackalloc T[previous.Length];
-            Span<T?> bufPrevRemains = stackalloc T?[previous.Length];
-            previous.CopyTo(bufPrev);
-            return execCore(current, bufPrev, bufPrevRemains, comparer);
+            return GetChangesSlow(current, previous, comparer);
         }
-        else
-        {
-            using var bufPrevArray = RentArray.RentAndCreate<T>(previous.Length);
-            using var bufPrevRemainsArray = RentArray.RentAndCreate<T?>(previous.Length);
-            Span<T> bufPrev = bufPrevArray.Span;
-            Span<T?> bufPrevRemains = bufPrevRemainsArray.Span;
-            previous.CopyTo(bufPrev);
-            return execCore(current, bufPrev, bufPrevRemains, comparer);
-        }
+        Span<T> bufPrev = stackalloc T[previous.Length];
+        Span<T?> bufPrevRemains = stackalloc T?[previous.Length];
+        previous.CopyTo(bufPrev);
+        return GetChangesCore(current, bufPrev, bufPrevRemains, comparer ?? Comparer<T>.Default);
+    }
 
-        static RentArray<ValueWithChange<T>> execCore(scoped ReadOnlySpan<T> current, scoped Span<T> prev, scoped Span<T?> prevRemains, IComparer<T> comparer)
-        {
-            prev.Sort(comparer);
-            prev.AsValueEnumerable().Select(x => new T?(x)).CopyTo(prevRemains);
+    public static RentArray<ValueWithChange<T>> GetChangesSlow<T>(this scoped ReadOnlySpan<T> current, scoped ReadOnlySpan<T> previous, IComparer<T>? comparer = default) where T : struct
+    {
+        using var bufPrevArray = RentArray.RentAndCreate<T>(previous.Length);
+        using var bufPrevRemainsArray = RentArray.RentAndCreate<T?>(previous.Length);
+        Span<T> bufPrev = bufPrevArray.Span;
+        Span<T?> bufPrevRemains = bufPrevRemainsArray.Span;
+        previous.CopyTo(bufPrev);
+        return GetChangesCore(current, bufPrev, bufPrevRemains, comparer ?? Comparer<T>.Default);
+    }
 
-            var result = RentArray.RentAndCreate<ValueWithChange<T>>(current.Length + prev.Length);
-            var resSpan = result.Span;
-            for (int i = 0; i < current.Length; i++)
+    static RentArray<ValueWithChange<T>> GetChangesCore<T>(scoped ReadOnlySpan<T> current, scoped Span<T> prev, scoped Span<T?> prevRemains, IComparer<T> comparer) where T : struct
+    {
+        prev.Sort(comparer);
+        prev.AsValueEnumerable().Select(x => (T?)x).CopyTo(prevRemains);
+
+        var result = RentArray.RentAndCreate<ValueWithChange<T>>(current.Length + prev.Length);
+        var resSpan = result.Span;
+        for (int i = 0; i < current.Length; i++)
+        {
+            var x = current[i];
+            var otherIdx = prev.BinarySearch(x, comparer);
+            if (otherIdx >= 0)
             {
-                var x = current[i];
-                var otherIdx = prev.BinarySearch(x, comparer);
-                if (otherIdx >= 0)
-                {
-                    resSpan[i] = new(x, Change.None);
-                    prevRemains[otherIdx] = null;
-                }
-                else
-                {
-                    resSpan[i] = new(x, Change.Added);
-                }
+                resSpan[i] = new(x, Change.None);
+                prevRemains[otherIdx] = null;
             }
-            int remCnt = 0;
-            var remSpan = resSpan[current.Length..];
-            foreach (var x in prevRemains.AsValueEnumerable().Where(y => y is not null))
+            else
             {
-                remSpan[remCnt++] = new(x!.Value, Change.Removed);
+                resSpan[i] = new(x, Change.Added);
             }
-            return result.Resize(current.Length + remCnt);
         }
+        int remCnt = 0;
+        var remSpan = resSpan[current.Length..];
+        foreach (var x in prevRemains.AsValueEnumerable().Where(y => y is not null))
+        {
+            remSpan[remCnt++] = new(x!.Value, Change.Removed);
+        }
+        return result.Resize(current.Length + remCnt);
     }
 }
