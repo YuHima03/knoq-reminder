a69a5fe89b99688fbac7228ab2a141b95c0baf1c
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2025-11-14 02:48:58 +0900

    ✨ DestinationDiscordWebhook and DestinationTraq implements IComparable and IEquatable

--- src/KnoqReminder.Domain/Repositories/Models/DestinationDiscordWebhook.cs
+++ src/KnoqReminder.Domain/Repositories/Models/DestinationDiscordWebhook.cs
@@ -1,8 +1,58 @@
 namespace KnoqReminder.Domain.Repositories.Models;
 
-public readonly struct DestinationDiscordWebhook
+public readonly struct DestinationDiscordWebhook : IComparable<DestinationDiscordWebhook>, IEquatable<DestinationDiscordWebhook>
 {
     public string WebhookId { get; init; }
 
     public string WebhookSecret { get; init; }
+
+    public int CompareTo(DestinationDiscordWebhook other)
+    {
+        return WebhookId.CompareTo(other.WebhookId);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is DestinationDiscordWebhook other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return WebhookId.GetHashCode(StringComparison.Ordinal);
+    }
+
+    public bool Equals(DestinationDiscordWebhook other)
+    {
+        return string.Equals(WebhookId, other.WebhookId, StringComparison.Ordinal);
+    }
+
+    public static bool operator ==(DestinationDiscordWebhook left, DestinationDiscordWebhook right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(DestinationDiscordWebhook left, DestinationDiscordWebhook right)
+    {
+        return !(left == right);
+    }
+
+    public static bool operator <(DestinationDiscordWebhook left, DestinationDiscordWebhook right)
+    {
+        return left.CompareTo(right) < 0;
+    }
+
+    public static bool operator <=(DestinationDiscordWebhook left, DestinationDiscordWebhook right)
+    {
+        return left.CompareTo(right) <= 0;
+    }
+
+    public static bool operator >(DestinationDiscordWebhook left, DestinationDiscordWebhook right)
+    {
+        return !(left <= right);
+    }
+
+    public static bool operator >=(DestinationDiscordWebhook left, DestinationDiscordWebhook right)
+    {
+        return !(left < right);
+    }
 }
