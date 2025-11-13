ddcb320aae894541daba9573411816f92e279a5b
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2025-11-13 19:42:20 +0900

    🎨 fmt

--- src/KnoqReminder.Domain/Repositories/Models/DestinationTraq.cs
+++ src/KnoqReminder.Domain/Repositories/Models/DestinationTraq.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace KnoqReminder.Domain.Repositories.Models;
 
 public readonly struct DestinationTraqChannel
--- src/KnoqReminder.Domain/Services/Reminder/IReminderPublisher.cs
+++ src/KnoqReminder.Domain/Services/Reminder/IReminderPublisher.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using KnoqReminder.Domain.Repositories.Models;
 using KnoqReminder.Domain.Services.Events;
 
 namespace KnoqReminder.Domain.Services.Reminder;
