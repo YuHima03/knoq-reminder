35175f1e0c2daab6873e010b759e38aff473a737
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2025-11-17 04:34:06 +0900

    ✨ StringHelper class

---
+++ src/KnoqReminder.Utilities/Helpers/StringHelper.cs
@@ -0,0 +1,13 @@
+namespace KnoqReminder.Utilities.Helpers;
+
+static class StringHelper
+{
+    public static string Truncate(this string s, int maxLength)
+    {
+        if (s is null || maxLength == 0)
+        {
+            return string.Empty;
+        }
+        return s.Length <= maxLength ? s : string.Concat(s.AsSpan(0, maxLength-1), "…");
+    }
+}
