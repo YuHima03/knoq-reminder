f2d8256027028b08352e947ba8fa16cbafa039b0
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2025-12-26 00:08:24 +0900

    ✨ implement IRepositoryProvider

--- src/KnoqReminder.Infrastructure/Database/Implements/RepositoryProviderImplement.cs
+++ src/KnoqReminder.Infrastructure/Database/Implements/RepositoryProviderImplement.cs
@@ -5,12 +5,29 @@ using Microsoft.EntityFrameworkCore;
 namespace KnoqReminder.Infrastructure.Database.Implements;
 
 sealed class RepositoryProviderImplement(
-    IDbContextFactory<AppDbContext>? appDbContextFactory
+    IDbContextFactory<AppDbContext> appDbContextFactory
     ) : IRepositoryProvider
 {
     public async ValueTask<TRepository> CreateRepositoryAsync<TRepository>(CancellationToken cancellationToken = default)
         where TRepository : class, IRepositoryBase
     {
-
+        AppDbContext? ctx = null;
+        try
+        {
+            ctx = await appDbContextFactory.CreateDbContextAsync(cancellationToken).ConfigureAwait(false);
+            if (ctx is not TRepository repository)
+            {
+                return ThrowHelper.ThrowInvalidOperationException<TRepository>($"The requested repository type '{typeof(TRepository).FullName}' is not supported.");
+            }
+            ctx = null; // Prevent disposal in finally block
+            return repository;
+        }
+        finally
+        {
+            if (ctx is not null)
+            {
+                await ctx.DisposeAsync().ConfigureAwait(false);
+            }
+        }
     }
 }
