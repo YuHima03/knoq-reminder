f908ca4ff1f899deea6673be74794c837aad3e3f
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2026-01-02 02:23:16 +0900

    🚚 rename

--- src/KnoqReminder.Domain/Options/IReminderOptions.cs
+++ src/KnoqReminder.Domain/Options/IReminderOptions.cs
@@ -2,7 +2,7 @@ namespace KnoqReminder.Domain.Options;
 
 public interface IReminderOptions
 {
-    TimeSpan AotReminderTimeBeforeEvent { get; }
+    TimeSpan AheadOfTimeReminderOffset { get; }
 
     TimeSpan SchedulingInterval { get; }
 }
