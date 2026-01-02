9b08d391bb386bfe070a9a2faa89da1e5af85c07
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2026-01-03 00:57:29 +0900

    ✨ implement IKnoqUrlProvider

---
+++ src/KnoqReminder.App/KnoqReminder.App/Services/Urls/KnoqUrlProvider.cs
@@ -0,0 +1,24 @@
+using Knoq;
+using KnoqReminder.Domain.Options;
+using KnoqReminder.Domain.Services.Urls;
+using Microsoft.Extensions.Options;
+
+namespace KnoqReminder.App.Services.Urls;
+
+sealed class KnoqUrlProvider(
+    IOptions<IKnoqClientOptions> options
+    )
+    : IKnoqUrlProvider
+{
+    public Uri BaseUrl { get; } = new(options.Value.WebPageBaseUrl);
+
+    public Uri GetEventPageUrl(Guid eventId)
+    {
+        return new Uri(BaseUrl, $"events/{eventId}");
+    }
+
+    public Uri GetGroupPageUrl(Guid groupId)
+    {
+        return new Uri(BaseUrl, $"groups/{groupId}");
+    }
+}
