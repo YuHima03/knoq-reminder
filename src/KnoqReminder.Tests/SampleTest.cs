8205c3335ec135fd69acd6532a510808d58dee72
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2026-01-04 12:32:35 +0900

    ✅ add SampleMethodTest

---
+++ src/KnoqReminder.Tests/SampleTest.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KnoqReminder.Tests;
+
+public class SampleTest
+{
+    [Fact]
+    public void SampleMethodTest()
+    {
+        return;
+    }
+}
