584ccf3fb7fe28e2399e3249e57a88246e926d30
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2026-01-06 17:38:17 +0900

    ✨ classes for providing configuration from .env files

---
+++ src/KnoqReminder.Utilities/Configuration/ConfigurationExtensions.cs
@@ -0,0 +1,27 @@
+using KnoqReminder.Utilities.Helpers;
+using Microsoft.Extensions.Configuration;
+using ZLinq;
+
+namespace KnoqReminder.Utilities.Configuration;
+
+public static class ConfigurationExtensions
+{
+    public static IConfigurationBuilder AddEnvFiles(this IConfigurationBuilder builder, bool areOptional, params ReadOnlySpan<string> paths)
+    {
+        foreach (var p in paths)
+        {
+            var fullPath = Path.GetFullPath(p);
+            if (!File.Exists(fullPath))
+            {
+                if (areOptional)
+                {
+                    continue;
+                }
+                GenericThrowHelper.Throw<FileNotFoundException>($"Configuration file not found: {fullPath}");
+            }
+            using var stream = File.OpenRead(fullPath);
+            builder.Add(new DotenvConfigurationSource() { Stream = stream });
+        }
+        return builder;
+    }
+}
