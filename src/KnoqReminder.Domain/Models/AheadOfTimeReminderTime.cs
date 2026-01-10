a5f02a419732fbe5f6acc1cffb51da352686c18e
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2026-01-10 18:50:00 +0900

    🐛 fix validation

--- src/KnoqReminder.Domain/Models/AheadOfTimeReminderTime.cs
+++ src/KnoqReminder.Domain/Models/AheadOfTimeReminderTime.cs
@@ -44,7 +44,7 @@ public readonly struct AheadOfTimeReminderTime : IComparable<AheadOfTimeReminder
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     static void ValidateConstructorArguments(int hours, int minutes)
     {
-        Guard.IsBetween(hours, 0, 24);
+        Guard.IsBetweenOrEqualTo(hours, 0, 24);
         if (hours != 24)
         {
             Guard.IsBetween(minutes, 0, 59);
