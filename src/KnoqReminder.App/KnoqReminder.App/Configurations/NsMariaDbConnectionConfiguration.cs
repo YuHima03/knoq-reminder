dda97468794abd312ba02073dc490a6bec92893e
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2026-01-12 03:17:52 +0900

    ✨ use alternative environment variables for db optios if possible

---
+++ src/KnoqReminder.App/KnoqReminder.App/Configurations/NsMariaDbConnectionConfiguration.cs
@@ -0,0 +1,52 @@
+using System.ComponentModel.DataAnnotations;
+using KnoqReminder.Domain.Options;
+using KnoqReminder.Infrastructure.Database;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace KnoqReminder.App.Configurations;
+
+public class NsMariaDbConnectionConfiguration : IDbConnectionOptions
+{
+    public const string Prefix = "NsMariadb";
+
+    readonly MariaDbConnectionConfiguration _config = new();
+
+    [ConfigurationKeyName(Prefix + "Hostname")]
+    public string? Host
+    {
+        get => _config.Host;
+        set => _config.Host = value;
+    }
+
+    [ConfigurationKeyName(Prefix + "Port")]
+    public int Port
+    {
+        get => _config.Port;
+        set => _config.Port = value;
+    }
+
+    [ConfigurationKeyName(Prefix + "User")]
+    public string? Username
+    {
+        get => _config.Username;
+        set => _config.Username = value;
+    }
+
+    [ConfigurationKeyName(Prefix + "Password")]
+    public string? Password
+    {
+        get => _config.Password;
+        set => _config.Password = value;
+    }
+
+    [ConfigurationKeyName(Prefix + "Database")]
+    public string? Database
+    {
+        get => _config.Database;
+        set => _config.Database = value;
+    }
+
+    public string ConnectionString => _config.ConnectionString;
+
+    public bool IsValid => Validator.TryValidateObject(_config, new ValidationContext(_config), null, validateAllProperties: true);
+}
--- src/KnoqReminder.App/KnoqReminder.App/Configurations/ServiceCollectionExtension.cs
+++ src/KnoqReminder.App/KnoqReminder.App/Configurations/ServiceCollectionExtension.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations;
 using KnoqReminder.Domain.Options;
+using KnoqReminder.Infrastructure.Database;
 using KnoqReminder.Utilities.Options;
+using Microsoft.Extensions.Options;
 
 namespace KnoqReminder.App.Configurations;
 
@@ -11,6 +14,27 @@ static class ServiceCollectionExtension
             .Bind(config.GetSection(TraqBotConfiguration.Position))
             .ValidateDataAnnotations()
             .ValidateOnStart();
+        return services;
+    }
+
+    public static IServiceCollection SetupRepository(this IServiceCollection services, IConfigurationRoot config, IHostEnvironment? environment = null)
+    {
+        services.AddRepository(environment);
+        services.ConfigureRepository(config, validation: false);
+        services.AddOptions<NsMariaDbConnectionConfiguration>()
+            .Bind(config);
+        services.AddSingleton<IOptions<IDbConnectionOptions>>(sp =>
+        {
+            var nsConfig = sp.GetRequiredService<IOptions<NsMariaDbConnectionConfiguration>>();
+            if (nsConfig.Value.IsValid)
+            {
+                return nsConfig;
+            }
+            var defaultConfig = sp.GetRequiredService<IOptions<MariaDbConnectionConfiguration>>();
+            var defaultConfigValue = defaultConfig.Value;
+            Validator.ValidateObject(defaultConfigValue, new ValidationContext(defaultConfigValue), validateAllProperties: true);
+            return defaultConfig;
+        });
         return services;
     }
 }
